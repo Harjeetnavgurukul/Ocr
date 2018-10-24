08627d13d0fb59b8a88595a3f322a60f80c0b78f
Your Name <harjeet17@navgurukul.org>
2018-10-24 17:37:08 +0530

    OCR

---
+++ app/src/main/java/com/recycler_view/ocr/OcrActivity.java
@@ -0,0 +1,13 @@
+package com.recycler_view.ocr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OcrActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ocr);
+    }
+}
